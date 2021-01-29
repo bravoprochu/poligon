3401ddbc78fb02e57376eddc21ee3fbebf8f19b5
Bartosz Prochowski <bravoprochu@gmail.com>
2021-01-29 11:37:20 +0100

    apiCoin lazy loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './sites/home/home.component';
 
 
 const routes: Routes = [
+  {path: 'home', component: HomeComponent},
   {path: 'coin-api', loadChildren: () => import('./other-modules/coin-api/coin-api/coin-api.module').then(m=>m.CoinApiModule)},
   
   {path: '', redirectTo: 'coin-api', pathMatch: 'full'},
