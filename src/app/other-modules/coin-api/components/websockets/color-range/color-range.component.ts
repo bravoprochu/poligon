2987906a4d90cb1ea7dfe6bcb23280d203b36fee
Bartosz Prochowski <bravoprochu@gmail.com>
2021-02-13 12:19:14 +0100

    color range comments to all conditions

--- src/app/other-modules/coin-api/components/websockets/color-range/color-range.component.ts
+++ src/app/other-modules/coin-api/components/websockets/color-range/color-range.component.ts
@@ -94,6 +94,7 @@ export class ColorRangeComponent implements OnInit {
     let newColors = [] as IColorRangeItem[];
 
     /**
+     * special case
      *  one set only values are larger then any actual min/max
      *
      */
@@ -113,19 +114,27 @@ export class ColorRangeComponent implements OnInit {
 
       if (min >= color.max) {
         /**
+         * "1"
          * not intersected on left side
-         * even min value can be set on item max (it will be FIX on the next item)
+         * even min value is current.max (it will be MANAGE on the next item)
+         * adds whole OLD item to new colors array
          *
          */
-        console.log(index, 'not intersected on left', color);
+        console.log(index, '1, not intersected on left', color);
         newColors.push(color);
       } else if (min === color.min && max <= color.max) {
         /**
-         * "1"
-         * min meets min but ends on the same item
+         * "2"
+         * min meets current item.min AND current.max
+         * is on the same range
          *
          */
-        console.log(index, '1', color);
+        console.log(index, '2', color);
+        /**
+         *"2a"
+         * pushes whole new range to new colors array
+         *
+         */
         newColors.push({
           color: newColor,
           min: min,
@@ -133,7 +142,9 @@ export class ColorRangeComponent implements OnInit {
         } as IColorRangeItem);
 
         /**
-         * if doesnt ends equally at the end, there is still old color item
+         * "2b"
+         * if doesnt ends equally at the end,
+         * there is still old color item to add
          *
          */
         if (max !== color.max) {
@@ -146,7 +157,10 @@ export class ColorRangeComponent implements OnInit {
       } else if (min > color.min && max < color.max) {
         /**
          * "3"
-         *
+         * new item is inside old item
+         * need to add 3 piecess. from item.min to new min,
+         * whole new range,
+         * and from new range max to item max...         *
          */
         console.log(index, '3', color);
         newColors.push({
@@ -168,10 +182,14 @@ export class ColorRangeComponent implements OnInit {
         } as IColorRangeItem);
       } else if (min > color.min && max === color.max) {
         /**
-         * "2"
+         * "4"
+         * whole new range is within current item
+         * but is greater then current.min
+         * need to add 2 pieces;
+         * old to new min and new range
          *
          */
-        console.log(index, '2', color);
+        console.log(index, '4', color);
         newColors.push({
           color: color.color,
           min: color.min,
@@ -181,14 +199,24 @@ export class ColorRangeComponent implements OnInit {
         newColors.push({
           color: newColor,
           min: min,
-          max: color.max,
+          max: max,
         } as IColorRangeItem);
       } else if (min >= color.min && min < color.max && max > color.max) {
         /**
-         * "4"
+         * "5"
+         * new min value is within current item
+         * but max is greater then current max
+         * need to add 2 piecess as "4",
+         * but here new max finishes on unknown next item
          *
          */
-        console.log(index, '4', color);
+        console.log(index, '5', color);
+        /**
+         * "5a"
+         * doesnt start at the beginnig of old item
+         * so, need to add a piece with old color
+         *
+         */
         if (min !== color.min) {
           newColors.push({
             color: color.color,
@@ -197,6 +225,11 @@ export class ColorRangeComponent implements OnInit {
           } as IColorRangeItem);
         }
 
+        /**
+         * "5b"
+         * adds whole new range
+         *
+         */
         newColors.push({
           color: newColor,
           min: min,
@@ -204,10 +237,13 @@ export class ColorRangeComponent implements OnInit {
         } as IColorRangeItem);
       } else if (min < color.min && max > color.min && max < color.max) {
         /**
-         * "5"
+         * "6"
+         * started at some previous item
+         * and end within current
+         * adds only rest of old color
          *
          */
-        console.log(index, '5', color);
+        console.log(index, '6', color);
         newColors.push({
           color: color.color,
           min: max,
@@ -215,14 +251,20 @@ export class ColorRangeComponent implements OnInit {
         } as IColorRangeItem);
       } else if (max <= color.min && max !== color.max) {
         /**
+         * "7"
          * not intersected on right side
+         * adds every item thats on the rigth side of new range
          *
          */
-        console.log(index, 'not intersected on right', color);
+        console.log(index, '7, not intersected on right', color);
         newColors.push(color);
       }
     }
 
+    /**
+     * replace colors array with new, fiexed one
+     *
+     */
     this.colors = newColors;
   }
 
