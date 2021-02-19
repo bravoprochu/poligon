727024c806e3d96e17f8e927c1007a4d1531755c
Bartosz Prochowski <bravoprochu@gmail.com>
2021-02-19 14:28:58 +0100

    indicator progressbar fix scss ext

--- src/app/other-modules/indicators/progress-bar/progress-bar.component.ts
+++ src/app/other-modules/indicators/progress-bar/progress-bar.component.ts
@@ -4,7 +4,7 @@ import { IndicatorsService } from '../indicators.service';
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.css'],
+  styleUrls: ['./progress-bar.component.scss'],
 })
 export class ProgressBarComponent implements OnInit {
   constructor(public indicatorsSrv: IndicatorsService) {}
