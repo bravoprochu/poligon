457b058cd2a3fc75e33655f09b93635b9ecf102f
Bartosz Prochowski <bravoprochu@gmail.com>
2021-02-24 14:35:36 +0100

    identAuth mocked login data response

--- src/app/other-modules/ident/services/ident-data-factory.service.ts
+++ src/app/other-modules/ident/services/ident-data-factory.service.ts
@@ -14,6 +14,36 @@ import { environment } from 'src/environments/environment';
 import { IndicatorsService } from '../../indicators/indicators.service';
 import { IIdentRegisterUser } from '../interfaces/i-ident-register-user';
 import { IIdentUser } from '../interfaces/i-ident-user';
+import { IUserToken } from '../interfaces/i-user-token';
+
+const USER_TOKEN_MOCKED = {
+  claims: [
+    {
+      type: 'sub',
+      value: 'bravoprochu@gmail.com',
+    },
+    {
+      type: 'jti',
+      value: 'c6e48acd-53dc-41e3-8940-09d3a4ead8e7',
+    },
+    {
+      type: 'nameid',
+      value: '329f5edb-6b24-4218-9845-a7fff5a29789',
+    },
+    {
+      type: 'roles',
+      value: 'Magazynier',
+    },
+    {
+      type: 'roles',
+      value: 'Administrator',
+    },
+  ],
+  expirationTime: '2021-02-24T16:08:20.9763257+01:00',
+  roles: ['Magazynier', 'Administrator'],
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJicmF2b3Byb2NodUBnbWFpbC5jb20iLCJqdGkiOiJjNmU0OGFjZC01M2RjLTQxZTMtODk0MC0wOWQzYTRlYWQ4ZTciLCJuYW1laWQiOiIzMjlmNWVkYi02YjI0LTQyMTgtOTg0NS1hN2ZmZjVhMjk3ODkiLCJyb2xlcyI6WyJNYWdhenluaWVyIiwiQWRtaW5pc3RyYXRvciJdLCJleHAiOjE2MTQxNzkzMDAsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.yYUGrJaZoni3lSaI7N9tC-0iIXuVIb3w_-eNUc65zk8',
+} as IUserToken;
 
 const RETRYWHEN_AND_FINALIZE_PIPE$ = (
   indicatorsService: IndicatorsService,
@@ -62,6 +92,42 @@ export class IdentDataFactoryService {
 
   loginUser(identUser: IIdentUser): Observable<any> {
     this.initIndicator();
+    /**
+     * check if mocked data is correct
+     *
+     */
+    if (
+      identUser.userName === 'testowo@gmail.com' &&
+      identUser.password === '123456'
+    ) {
+      const mockedUser = USER_TOKEN_MOCKED;
+      /**
+       * update expiration time
+       *
+       */
+      mockedUser.expirationTime = new Date(
+        new Date().getTime() + 60 * 60000
+      ).toUTCString();
+
+      return of(mockedUser).pipe(
+        tap(() =>
+          this.indicatorsService.message(
+            'login user',
+            'faking getting data from server..'
+          )
+        ),
+        delay(2500),
+        finalize(() => {
+          this.indicatorsService.isInProgress$.next(false);
+          this.indicatorsService.message('login user', 'You are logged in !!');
+        })
+      );
+    }
+
+    /**
+     *
+     *
+     */
     return this.httpClient
       .post(environment.identTokenUrl, identUser)
       .pipe(RETRYWHEN_AND_FINALIZE_PIPE$(this.indicatorsService));
