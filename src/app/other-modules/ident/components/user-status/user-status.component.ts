accd82804d45b4be748b3f75f7249eededf65b89
Bartosz Prochowski <bravoprochu@gmail.com>
2021-02-24 23:31:33 +0100

    logs user status logout button, display fix

--- src/app/other-modules/ident/components/user-status/user-status.component.ts
+++ src/app/other-modules/ident/components/user-status/user-status.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil, tap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+
 import { IUserToken } from '../../interfaces/i-user-token';
 import { LoginService } from '../../services/login.service';
 
@@ -9,37 +8,17 @@ import { LoginService } from '../../services/login.service';
   templateUrl: './user-status.component.html',
   styleUrls: ['./user-status.component.scss'],
 })
-export class UserStatusComponent implements OnInit, OnDestroy {
+export class UserStatusComponent implements OnInit {
   constructor(private loginService: LoginService) {}
 
-  isLoggedIn = false;
-  isDestroyed$: Subject<boolean> = new Subject();
+  ngOnInit(): void {}
 
-  ngOnDestroy(): void {
-    this.isDestroyed$.next(true);
-    this.isDestroyed$.complete();
-    this.isDestroyed$.unsubscribe();
+  logout(): void {
+    this.loginService.logout();
   }
 
-  ngOnInit(): void {
-    this.initObservable();
-  }
-
-  initObservable(): void {
-    this.loginService.isLoggedIn$
-      .pipe(
-        takeUntil(this.isDestroyed$),
-        tap((isLoggedIn: boolean) => {
-          this.isLoggedIn = isLoggedIn;
-        })
-      )
-      .subscribe(
-        (isLoggedIn: any) => {
-          console.log('isLoggedIn subs:', isLoggedIn);
-        },
-        (error) => console.log('isLoggedIn error', error),
-        () => console.log('isLoggedIn completed..')
-      );
+  get isLoggedIn(): boolean {
+    return this.loginService.isLoggedIn;
   }
 
   get loggedInUser(): IUserToken {
